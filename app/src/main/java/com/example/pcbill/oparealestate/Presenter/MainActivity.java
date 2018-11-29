b1323591305b404f2e77ee7027396ecf8ba82676
billk97 <bill1897@yahoo.gr>
2018-11-29 19:44:09 +0200

    First Oficial Push

---
+++ app/src/main/java/com/example/pcbill/oparealestate/Presenter/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.pcbill.oparealestate.Presenter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pcbill.oparealestate.Controler.DBHelper;
+import com.example.pcbill.oparealestate.R;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button Search;
+    Button Sale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeVariables();
+        DBHelper db = new DBHelper();
+        try {
+
+
+            db.CreateTable();
+            Toast toast =Toast.makeText(getApplicationContext(), "created1",Toast.LENGTH_LONG);
+            toast.show();
+
+        } catch (Exception e) {
+            Toast toast =Toast.makeText(getApplicationContext(), (CharSequence) e,Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent myIntent = new Intent(view.getContext(),onoma);
+                //startActivityForResult(myIntent,0);
+            }
+        });
+        Sale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(),Sale.class);
+                startActivityForResult(myIntent,0);
+            }
+        });
+
+    }
+    private void initializeVariables()
+    {
+        textView=(TextView) findViewById(R.id.MainTextView);
+        Search=(Button)findViewById(R.id.Searchbutton);
+        Sale=(Button)findViewById(R.id.Salebutton);
+    }
+}
